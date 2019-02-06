2d4707dbc9c2afa8864e42945cd54c6d65fddda2
William <wpdcastro@gmail.com>
2019-02-06 21:31:34 -0200

    nova pasta

---
+++ novo/Node.php
@@ -0,0 +1,21 @@
+<?php
+class Node {
+	public $upperNode;
+	public $freq;
+	public $left;
+	public $right;
+	public $letter;
+
+	public function __construct ($freq = NULL, $letter = NULL, $left = NULL, $right = NULL)
+	{
+		$this->freq = $freq;
+		$this->letter = $letter;
+		$this->left = $left;
+		$this->right = $right;
+	}
+}
+
+
+
+
+//comentario
